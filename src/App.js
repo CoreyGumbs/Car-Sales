6889e88f16f54944d905b9f58b0191cd87d437b0
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-22 18:13:40 -0500

    [Add] Moved Reducer, Store, etc. to index.js from app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import { createStore} from 'redux';
 
-
-import {initiailState, carReducer } from './reducers/carReducer';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const store = createStore(carReducer);
+
 
 const App = () => {
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore} from 'redux';
 import {Provider} from 'react-redux';
+
+import {initiailState, carReducer } from './reducers/carReducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+const store = createStore(carReducer);
+console.log(store.getState());
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>
 
